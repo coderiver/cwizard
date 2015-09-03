b9b6d1ddc57d390d1958b9b951c7a79c025625ab
moroz <arturmorozv@gmail.com>
2015-09-03 17:50:01 +0300

    updates

--- js/common.js
+++ js/common.js
@@ -1,19 +1,96 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// window resize
+	$(window).resize(function () {
+		var windowWidth = $(window).width();
+		// section
+		countSection();
+	});
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// steps
+	(function () {
+		var steps   = $('.js-steps'),
+			btn     = steps.find('.js-steps-btn'),
+			item    = steps.find('.js-steps-item'),
+			section = $('.js-section');
+		btn.on('click', function () {
+			var _this = $(this),
+				index = _this.index();
+			if (!_this.hasClass('is-active')) {
+				btn.removeClass('is-active');
+				_this.addClass('is-active');
+				item.hide();
+				item.eq(index).show();
+				if (index == 2) {
+					section.addClass('is-list-show');
+				}
+				else {
+					section.removeClass('is-list-show');
+				}
+			};
+		});
+	}());
 
-	console.log($('body').html());
+	// section body scroll
+	(function () {
+		var section = $('.js-section'),
+			body    = section.find('.js-section-body');
+		body.scroll(function () {
+			var posScroll = body.scrollTop();
+			if (posScroll > 0) {
+				section.addClass('is-scroll');
+			}
+			else {
+				section.removeClass('is-scroll');
+			}
+		});
+	}());
+
+	// section
+	function countSection () {
+		var section    = $('.js-section'),
+			head       = section.find('.js-section-head'),
+			headHeight = head.height(),
+			body       = section.find('.js-section-body');
+		body.css('top', headHeight);
+	};
+	countSection();
+
+	// range
+	(function () {
+		var range = $('.js-range');
+		if (range.length) {
+			range.each(function () {
+				$(this).slider({
+					range: true,
+					min: 0,
+					max: 100,
+					values: [ 25, 75 ],
+					slide: function( event, ui ) {
+						// ui.values[0]
+						// ui.values[1]
+					}
+				});
+			});
+		};
+	}());
+
+	// university slider
+	(function () {
+		var sl   = $('.js-university-slider'),
+			prev = $('.js-university-prev'),
+			next = $('.js-university-next');
+		if (sl.length) {
+			sl.slick({
+				arrows: false
+			});
+			prev.on('click', function () {
+				sl.slick('prev');
+			});
+			next.on('click', function () {
+				sl.slick('next');
+			});
+		};
+	}());
+
 });
